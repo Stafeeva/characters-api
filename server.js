bc2f9a99e479c369aefb55b8b553eb852da77354
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-06-16 20:39:01 +0100

    Add body-parser

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
+
+var characters = [
+    {
+        name: "Luke Skywalker",
+        height: 172,
+        mass: 77,
+        hair_color: "blond",
+        skin_color: "fair",
+        eye_color: "blue",
+        birth_year: "19BBY",
+        is_male: true
+    }
+]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/characters', (req, res) => {
-  res.send('List of characters to go here')
+  console.log('listing characters')
+  res.json(characters)
+})
+
+app.post('/characters', (req, res) => {
+  characters.push(req.body)
+  res.send('Create character to go here')
 })
 
 app.listen(3000, () => {
