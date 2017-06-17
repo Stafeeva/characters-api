5def8b37ced4e61b6284d5fc144697ccc3af96f5
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-06-17 15:07:45 +0100

    Set up react, babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  entry: './ui/ui.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  }
+};
