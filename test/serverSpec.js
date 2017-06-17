de2d143deffc8c422ccbdc3074a0df44c55038c9
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-06-17 09:31:16 +0100

    Set up mocha and create app.js

---
+++ test/serverSpec.js
@@ -0,0 +1,21 @@
+const assert = require('assert')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../src/app.js').app
+const should = chai.should()
+
+chai.use(chaiHttp)
+
+describe('app', () => {
+  describe('GET /', () => {
+    it("should return 'Hello World!'", done => {
+      chai.request(app)
+          .get('/')
+          .end((err, res) => {
+            res.should.have.status(200)
+            res.text.should.equal('Hello World!')
+            done()
+          })
+    })
+  })
+})
--- test/test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert')
-
-describe('test', () => {
-  it('should blah', () => {
-    assert.equal(100, 105)
-  })
-})
