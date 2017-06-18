97f081c888e623c88873ed8204bd1f196b511625
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-06-18 11:54:10 +0100

    Add form to create character

--- ui/AddCharacter.js
+++ ui/AddCharacter.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
-import { Link } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 
 class AddCharacter extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {name: ''}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    const data = this.state
+    axios.post('/character', data)
+         .then(response => {
+           this.props.history.push('/')
+         })
+  }
+
   render() {
     return <div>
-      <h2>Add character here</h2>
-
+      <h2>Add character</h2>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Add character" />
+      </form>
       <Link to="/">Back</Link>
     </div>
   }
 }
 
-export default AddCharacter
+export default withRouter(AddCharacter)
