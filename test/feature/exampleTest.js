f4cfd29e108136ba8fe0a5cc2238d98fd3df7044
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-06-21 20:48:30 +0100

    Update example test

--- test/feature/exampleTest.js
+++ test/feature/exampleTest.js
@@ -1,8 +1,10 @@
-const webdriver = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver')
+const By = webdriver.By
 
 const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
 // ask the browser to open a page
-driver.navigate().to('http://localhost:3000');
+driver.navigate().to('http://localhost:3000')
+driver.findElement(By.css('.addButton')).click()
